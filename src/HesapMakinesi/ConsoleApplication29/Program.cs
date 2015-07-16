dae7bd625fa980c8d91b1464fb791d8c8601a05b
BETÜL HALICI <betul.halici@gmail.com>
2015-07-16 09:17:20 +0300

    no message

--- src/HesapMakinesi/ConsoleApplication29/Program.cs
+++ src/HesapMakinesi/ConsoleApplication29/Program.cs
@@ -13,41 +13,54 @@ namespace ConsoleApplication29
 
             string s1, s2,islem;
             double sayi1, sayi2,sonuc=0;
+            int n=0;
 
-            Console.Write("ilk sayıyı girin:");
-            s1 = Console.ReadLine();
-            Console.Write("ikinci sayıyı girin:");
-            s2 = Console.ReadLine();
-
-            sayi1 = Convert.ToDouble(s1);
-            sayi2 = Convert.ToDouble(s2);
-            Console.WriteLine("\n1-toplama\n2-çıkarma\n3-çarpma\n4-bölme\n\n islemi seciniz: ");
-            islem = Console.ReadLine();
-
-            switch(islem)
+            do
             {
-                case "1":
-                    sonuc = sayi1 + sayi2;
-                    break;
+                Console.Write("ilk sayıyı girin:");
+                s1 = Console.ReadLine();
+                Console.Write("ikinci sayıyı girin:");
+                s2 = Console.ReadLine();
 
-                case "2":
-                    sonuc = sayi1 - sayi2;
+                sayi1 = Convert.ToDouble(s1);
+                sayi2 = Convert.ToDouble(s2);
+                Console.WriteLine("\n1-toplama\n2-çıkarma\n3-çarpma\n4-bölme\n5-çıkış\n");
+                Console.WriteLine("İşlemi seçiniz:");
+                islem = Console.ReadLine();
+                if (islem == "27")
                     break;
+                switch (islem)
+                {
+                    case "1":
+                        sonuc = sayi1 + sayi2;
+                        Console.Write("{0}+{1}={2}",sayi1,sayi2,sonuc);
+                        Console.ReadLine();
+                        break;
+                    case "2":
+                        sonuc = sayi1 - sayi2;
+                        Console.Write("{0}-{1}={2}", sayi1, sayi2, sonuc);
+                        Console.ReadLine();
+                        break;
 
-                case "3":
-                    sonuc = sayi1 * sayi2;
-                    break;
+                    case "3":
+                        sonuc = sayi1 * sayi2;
+                        Console.Write("{0}*{1}={2}", sayi1, sayi2, sonuc);
+                        Console.ReadLine();
+                        break;
 
-                case "4":
-                    sonuc = sayi1 / sayi2;
-                    break;
+                    case "4":
+                        sonuc = sayi1 / sayi2;
+                        Console.Write("{0}/{1}={2}", sayi1, sayi2, sonuc);
+                        Console.ReadLine();
+                        break;
 
-                default:
-                    Console.WriteLine("hatalı secim");
-                    break;
-            }
-            Console.Write("\nSonuç: " + sonuc);
-            Console.ReadLine();
+              
+                    default:
+                        Console.WriteLine("hatalı secim");
+                        break;
+                }
+              
+            } while (n !=27);
             
         }
     }
