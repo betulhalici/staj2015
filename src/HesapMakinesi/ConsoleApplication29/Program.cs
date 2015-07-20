b2bad64101e3616b4b02c8b6486c04c05b9208f9
BETÜL HALICI <betul.halici@gmail.com>
2015-07-20 16:50:38 +0300

    no message

--- src/HesapMakinesi/ConsoleApplication29/Program.cs
+++ src/HesapMakinesi/ConsoleApplication29/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,58 +12,81 @@ namespace ConsoleApplication29
     {
         static void Main(string[] args)
         {
-
-            string s1, s2,islem;
-            double sayi1, sayi2,sonuc=0;
-            int n=0;
+            int n = 0;
+            string s1, s2, islem;
+            double sayi1 = 0.0, sayi2 = 0.0, sonuc = 0;
 
             do
             {
-                Console.Write("ilk sayıyı girin:");
-                s1 = Console.ReadLine();
-                Console.Write("ikinci sayıyı girin:");
-                s2 = Console.ReadLine();
+                try
+                {
+                    Console.Write("ilk sayıyı girin:");
+                    s1 = Console.ReadLine();
 
-                sayi1 = Convert.ToDouble(s1);
-                sayi2 = Convert.ToDouble(s2);
+                    Console.Write("ikinci sayıyı girin:");
+                    s2 = Console.ReadLine();
+
+                    sayi1 = Convert.ToDouble(s1);
+                    sayi2 = Convert.ToDouble(s2);
+                }
+                catch (Exception hata)
+                {
+                    Console.WriteLine("Hata: " + hata.Message);
+                    Console.WriteLine("Tekrar deneyiniz ve lütfen sayı giriniz.");
+
+                    Console.Write("\nilk sayıyı girin:");
+                    s1 = Console.ReadLine();
+
+                    Console.Write("ikinci sayıyı girin:");
+                    s2 = Console.ReadLine();
+
+                    sayi1 = Convert.ToDouble(s1);
+                    sayi2 = Convert.ToDouble(s2);
+
+                }
+
                 Console.WriteLine("\n1-toplama\n2-çıkarma\n3-çarpma\n4-bölme\n5-çıkış\n");
+
                 Console.WriteLine("İşlemi seçiniz:");
                 islem = Console.ReadLine();
+
                 if (islem == "27")
                     break;
                 switch (islem)
                 {
+
                     case "1":
                         sonuc = sayi1 + sayi2;
-                        Console.Write("{0}+{1}={2}",sayi1,sayi2,sonuc);
+                        Console.Write("{0}+{1}={2}\n", sayi1, sayi2, sonuc);
                         Console.ReadLine();
                         break;
                     case "2":
                         sonuc = sayi1 - sayi2;
-                        Console.Write("{0}-{1}={2}", sayi1, sayi2, sonuc);
+                        Console.Write("{0}-{1}={2}\n", sayi1, sayi2, sonuc);
                         Console.ReadLine();
                         break;
 
                     case "3":
                         sonuc = sayi1 * sayi2;
-                        Console.Write("{0}*{1}={2}", sayi1, sayi2, sonuc);
+                        Console.Write("{0}*{1}={2}\n", sayi1, sayi2, sonuc);
                         Console.ReadLine();
                         break;
 
                     case "4":
                         sonuc = sayi1 / sayi2;
-                        Console.Write("{0}/{1}={2}", sayi1, sayi2, sonuc);
+                        Console.Write("{0}/{1}={2}\n", sayi1, sayi2, sonuc);
                         Console.ReadLine();
                         break;
 
-              
+
                     default:
                         Console.WriteLine("hatalı secim");
                         break;
                 }
-              
-            } while (n !=27);
-            
+
+            } while (n != 27);
+            Console.ReadLine();
+
         }
     }
 }
