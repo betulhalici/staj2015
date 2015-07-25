82724173e047abd6544342e8416b8f9cd17b3c4d
betulhalici <betul.halici@gmail.com>
2015-07-25 17:33:28 +0300

    Update Program.cs

--- src/HesapMakinesi/ConsoleApplication29/Program.cs
+++ src/HesapMakinesi/ConsoleApplication29/Program.cs
@@ -10,89 +10,92 @@ namespace ConsoleApplication29
 {
     class Program
     {
+        static double SayiGirisi(string soru)
+        {
+           for(; ;)
+           {
+               try
+               {
+                  Console.WriteLine(soru);
+                  string kullaniciGirisi=Console.ReadLine();
+                  return Convert.ToDouble(kullaniciGirisi);
+                }
+                catch(Exception)
+                {
+                }
+            }
+        }
+        static string İslemSec()
+        {
+            Console.WriteLine("\n1-toplama\n2-çıkarma\n3-çarpma\n4-bölme\n5-çıkış\n");
+            Console.WriteLine("İşlemi seçiniz:");
+            return Console.ReadLine();
+        }
+
+        static void İslemYap(string islem, double sayi1, double sayi2)
+        {
+            switch (islem)
+            {
+                case "1":
+                    Console.WriteLine("Toplama: {0}", Toplama(sayi1, sayi2));
+                    break;
+
+                case "2":
+                    Console.WriteLine("Çıkarma: {0}", Çıkarma(sayi1, sayi2));
+                    break;
+
+                case "3":
+                    Console.WriteLine("Çarpma: {0}", Çarpma(sayi1, sayi2));
+                    break;
+
+                case "4":
+                    Console.WriteLine("Bölme: {0}", Bölme(sayi1, sayi2));
+                    break;
+            }
+        }
+        static double Toplama(double sayi1, double sayi2)
+        {
+            double toplama = sayi1 + sayi2;
+            return toplama;
+        }
+
+        static double Çıkarma(double sayi1, double sayi2)
+        {
+            double çıkarma = sayi1 - sayi2;
+            return çıkarma;
+        }
+
+        static double Çarpma(double sayi1, double sayi2)
+        {
+            double çarpma = sayi1 * sayi2;
+            return çarpma;
+        }
+
+        static double Bölme(double sayi1, double sayi2)
+        {
+            double bölme = sayi1 / sayi2;
+            return bölme;
+        }
+         
         static void Main(string[] args)
         {
             int n = 0;
-            string s1, s2, islem;
-            double sayi1 = 0.0, sayi2 = 0.0, sonuc = 0;
+            string islem;
+            double sayi1 = 0.0, sayi2 = 0.0;
 
             do
             {
-                for (; ; )
-                {
-                    try
-                    {
-                        Console.Write("ilk sayıyı girin:");
-                        s1 = Console.ReadLine();
-                        sayi1 = Convert.ToDouble(s1);
-                        break;
-                    }
-
-                    catch (Exception)
-                    {
-                        continue;
-                    }
-                 }
-
-                for (; ; )
-                {
-                    try
-                    {
-                        Console.Write("ikinci sayıyı girin:");
-                        s2 = Console.ReadLine();
-                        sayi2 = Convert.ToDouble(s2);
-                        break;
-                     }
-
-                    catch (Exception)
-                    {
-                        continue;
-                    }
+                sayi1=SayiGirisi("ilk sayıyı girin:");
+                sayi2=SayiGirisi("ikinci sayıyı girin:");
                
-                }
+                islem = İslemSec();
 
-                Console.WriteLine("\n1-toplama\n2-çıkarma\n3-çarpma\n4-bölme\n5-çıkış\n");
-
-                Console.WriteLine("İşlemi seçiniz:");
-                islem = Console.ReadLine();
-
-                if (islem == "27")
+               if (islem == "27")
                     break;
 
-                switch (islem)
-                {
-                    case "1":
-                        sonuc = sayi1 + sayi2;
-                        Console.Write("{0}+{1}={2}\n", sayi1, sayi2, sonuc);
-                        Console.ReadLine();
-                        break;
-
-                    case "2":
-                        sonuc = sayi1 - sayi2;
-                        Console.Write("{0}-{1}={2}\n", sayi1, sayi2, sonuc);
-                        Console.ReadLine();
-                        break;
-
-                    case "3":
-                        sonuc = sayi1 * sayi2;
-                        Console.Write("{0}*{1}={2}\n", sayi1, sayi2, sonuc);
-                        Console.ReadLine();
-                        break;
-
-                    case "4":
-                        sonuc = sayi1 / sayi2;
-                        Console.Write("{0}/{1}={2}\n", sayi1, sayi2, sonuc);
-                        Console.ReadLine();
-                        break;
-
-                    default:
-                        Console.WriteLine("hatalı secim");
-                        break;
-                }
-
+               İslemYap(islem, sayi1, sayi2);
             } while (n != 27);
-            Console.ReadLine();
-          }
-      }
- }
+        }
+    }
+}
 
